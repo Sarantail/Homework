65e72615568afbe7eac9039c06b987a6dcd28796
sarantail <sarantail@gmail.com>
2022-08-22 20:56:18 +0300

    homework

---
+++ Lesson4/Exs25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5-> 243(3⁵)
+// 2, 4-> 16
+
+Console.WriteLine("Введите число A");
+string? numberString1 = Console.ReadLine();
+var A = int.Parse(numberString1);
+
+Console.WriteLine("Введите число B");
+string? numberString2 = Console.ReadLine();
+var B = int.Parse(numberString2);
+
+int result = A;
+for (int i = 1; i < B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine(result);
---
+++ Lesson4/Exs27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452-> 11
+// 82-> 10
+// 9012-> 12
+
+Console.WriteLine("Введите число");
+string? numberString = Console.ReadLine();
+var SumNum = int.Parse(numberString);
+
+int result = 0;
+
+while (SumNum > 0)
+{
+    int num = SumNum % 10;
+    SumNum = SumNum / 10;
+    result = result + num;
+}
+
+Console.WriteLine(result);
