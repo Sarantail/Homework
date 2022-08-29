3e18fa5acf99d23d5d916aa84b85e7866e3fc18b
sarantail <sarantail@gmail.com>
2022-08-29 10:57:38 +0300

    homework

---
+++ Lesson6/41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Введите M чисел: ");
+int[] M = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int count = 0;
+
+for (int i = 0; i < M.Length; i++)
+{
+    if (M[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
---
+++ Lesson6/43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0,5; -0,5)
+
+Console.Write("Введите b1: ");
+string? B1 = Console.ReadLine();
+int b1 = int.Parse(B1);
+Console.Write("Введите k1: ");
+string? K1 = Console.ReadLine();
+int k1 = int.Parse(K1);
+Console.Write("Введите b2: ");
+string? B2 = Console.ReadLine();
+int b2 = int.Parse(B2);
+Console.Write("Введите k2: ");
+string? K2 = Console.ReadLine();
+int k2 = int.Parse(K2);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+
+Console.WriteLine($"Точка пересечения: ({x}; {y})");
+
